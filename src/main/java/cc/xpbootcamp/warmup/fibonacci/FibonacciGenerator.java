428be46e9c2669906f981b9ee230b9d74e042f2e
YONGMA <yong.ma@thoughtworks.com>
2020-02-17 13:16:30 +0800

    添加测试：When位数为1, then求值,返回1，并且添加Fibonacci数列产生器类

---
+++ src/main/java/cc/xpbootcamp/warmup/fibonacci/FibonacciGenerator.java
@@ -0,0 +1,7 @@
+package cc.xpbootcamp.warmup.fibonacci;
+
+public class FibonacciGenerator {
+    public int getFibonacci(int i) {
+        return 1;
+    }
+}
---
+++ src/test/java/cc/xpbootcamp/warmup/fibonacci/FibonacciGeneratorTest.java
@@ -0,0 +1,27 @@
+package cc.xpbootcamp.warmup.fibonacci;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class FibonacciGeneratorTest {
+
+    private FibonacciGenerator fibonacciGenerator;
+
+    @BeforeEach
+    void setUp() {
+        fibonacciGenerator = new FibonacciGenerator();
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    public void should_return_1_when_calculate_given_position_is_1() {
+        int fibonacciNumber = fibonacciGenerator.getFibonacci(1);
+        assertEquals(1, fibonacciNumber);
+    }
+}
