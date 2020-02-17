723b431319a6bb5d8a4f9240a57b26290388d88c
YONGMA <yong.ma@thoughtworks.com>
2020-02-17 13:31:03 +0800

    添加测试：Given位数为50, When求值,Then返回12586269025L

--- src/main/java/cc/xpbootcamp/warmup/fibonacci/FibonacciGenerator.java
+++ src/main/java/cc/xpbootcamp/warmup/fibonacci/FibonacciGenerator.java
@@ -1,7 +1,7 @@
 package cc.xpbootcamp.warmup.fibonacci;
 
 public class FibonacciGenerator {
-    public int getFibonacci(int i) {
+    public long getFibonacci(int i) {
         if (i == 1 || i == 2)
             return 1;
         return getFibonacci(i - 1) + getFibonacci(i - 2);
--- src/test/java/cc/xpbootcamp/warmup/fibonacci/FibonacciGeneratorTest.java
+++ src/test/java/cc/xpbootcamp/warmup/fibonacci/FibonacciGeneratorTest.java
@@ -21,31 +21,37 @@ class FibonacciGeneratorTest {
 
     @Test
     public void should_return_1_when_calculate_given_position_is_1() {
-        int fibonacciNumber = fibonacciGenerator.getFibonacci(1);
+        long fibonacciNumber = fibonacciGenerator.getFibonacci(1);
         assertEquals(1, fibonacciNumber);
     }
 
     @Test
     public void should_return_1_when_calculate_given_position_is_2() {
-        int fibonacciNumber = fibonacciGenerator.getFibonacci(2);
+        long fibonacciNumber = fibonacciGenerator.getFibonacci(2);
         assertEquals(1, fibonacciNumber);
     }
 
     @Test
     public void should_return_2_when_calculate_given_position_is_3() {
-        int fibonacciNumber = fibonacciGenerator.getFibonacci(3);
+        long fibonacciNumber = fibonacciGenerator.getFibonacci(3);
         assertEquals(2, fibonacciNumber);
     }
 
     @Test
     public void should_return_3_when_calculate_given_position_is_4() {
-        int fibonacciNumber = fibonacciGenerator.getFibonacci(4);
+        long fibonacciNumber = fibonacciGenerator.getFibonacci(4);
         assertEquals(3, fibonacciNumber);
     }
 
     @Test
     public void should_return_144_when_calculate_given_position_is_12() {
-        int fibonacciNumber = fibonacciGenerator.getFibonacci(12);
+        long fibonacciNumber = fibonacciGenerator.getFibonacci(12);
         assertEquals(144, fibonacciNumber);
+    }
+
+    @Test
+    public void should_return_12586269025_when_calculate_given_position_is_50() {
+        long fibonacciNumber = fibonacciGenerator.getFibonacci(50);
+        assertEquals(12586269025L, fibonacciNumber);
     }
 }
