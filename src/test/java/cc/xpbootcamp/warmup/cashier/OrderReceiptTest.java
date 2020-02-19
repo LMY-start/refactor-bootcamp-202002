7b0e30ec3ea20a6c3fbf4bdee11c17841f00d660
YONGMA <yong.ma@thoughtworks.com>
2020-02-19 11:57:13 +0800

    添加测试：全部发票打印内容是否符合格式要求

--- src/test/java/cc/xpbootcamp/warmup/cashier/OrderReceiptTest.java
+++ src/test/java/cc/xpbootcamp/warmup/cashier/OrderReceiptTest.java
@@ -7,10 +7,9 @@ import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.List;
 
-import static cc.xpbootcamp.warmup.cashier.Constant.DIVIDING_LINE;
-import static cc.xpbootcamp.warmup.cashier.Constant.NEW_HEADER;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.containsString;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class OrderReceiptTest {
     @Test
@@ -49,7 +48,7 @@ class OrderReceiptTest {
         String output = receipt.printNewReceipt();
         String weekday = WeekDay.of(order.date).getValue();
 
-        assertThat(output, containsString(NEW_HEADER));
+        assertThat(output, containsString("===== 老王超市，值得信赖 ======\n"));
         assertThat(output, containsString(weekday));
     }
 
@@ -65,16 +64,16 @@ class OrderReceiptTest {
 
         assertThat(output, containsString("巧克力, 21.50 x 2, 43.00\n"));
         assertThat(output, containsString("小白菜, 10.00 x 1, 10.00\n"));
-        assertThat(output, containsString(DIVIDING_LINE));
+        assertThat(output, containsString("-----------------------------------\n"));
     }
 
     @Test
-    public void should_print_totalSalesTax_and_totalAmount_items_when_print_new_receipt_give_a_order() {
+    public void should_print_totalSalesTax_and_totalAmount_items_when_print_new_receipt_give_a_order() throws ParseException {
         List<LineItem> lineItems = new ArrayList<LineItem>() {{
             add(new LineItem("巧克力", 21.5, 2));
             add(new LineItem("小白菜", 10.0, 1));
         }};
-        OrderReceipt receipt = new OrderReceipt(new Order(null, null, lineItems));
+        OrderReceipt receipt = new OrderReceipt(new Order(null, null, lineItems, new SimpleDateFormat("yyyy-MM-dd").parse("2020-2-18")));
 
         String output = receipt.printNewReceipt();
 
@@ -98,5 +97,27 @@ class OrderReceiptTest {
         assertThat(output, containsString("总价: 57.13\n"));
     }
 
+    @Test
+    public void should_print_all_right_when_print_new_receipt_give_a_order_with_date_is_Wednesday() throws ParseException {
+        List<LineItem> lineItems = new ArrayList<LineItem>() {{
+            add(new LineItem("巧克力", 21.5, 2));
+            add(new LineItem("小白菜", 10.0, 1));
+        }};
+
+        OrderReceipt receipt = new OrderReceipt(new Order(null, null, lineItems, new SimpleDateFormat("yyyy-MM-dd").parse("2020-2-19")));
+        String expected = "===== 老王超市，值得信赖 ======\n"
+                + "\n"
+                + "2020年2月19日，星期三\n"
+                + "\n"
+                + "巧克力, 21.50 x 2, 43.00\n"
+                + "小白菜, 10.00 x 1, 10.00\n"
+                + "-----------------------------------\n"
+                + "税额: 5.30\n"
+                + "折扣: 1.17\n"
+                + "总价: 57.13\n";
+        String output = receipt.printNewReceipt();
+
+        assertEquals(expected, output);
+    }
 
 }
